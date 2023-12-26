4373badd0808279a8c4112df64ad5b647959156a
Anastasia Savinova <nsty.nirvana@gmail.com>
2023-12-26 12:39:49 +0300

    поправить характеристики карточек и стиль заголовков в листе

--- local/templates/komsys/components/bitrix/news.list/products-list/template.php
+++ local/templates/komsys/components/bitrix/news.list/products-list/template.php
@@ -2,7 +2,7 @@
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
-/** @global CUser $USER */
+global $USER;
 /** @global CDatabase $DB */
 /** @var CBitrixComponentTemplate $this */
 /** @var string $templateName */
@@ -23,10 +23,33 @@ $this->setFrameMode(true);
                                         class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt=""
                                         loading="lazy"></div>
                 <div class="title"><?= $arItem["NAME"] ?></div>
+
             </a>
+            <?if($USER -> isAdmin()):?>
+                <div class="bx-filter-param-text">
+                    <span><?= $arItem["PROPERTIES"]["PURPOSE_OF_SEWER"]["VALUE"] ?>.</span>
+                    <?if(!empty($arItem["PROPERTIES"]["DIAMETER"]["VALUE"])):?>
+                    <span><?= $arItem["PROPERTIES"]["DIAMETER"]["VALUE"] ?> мм.</span>
+                    <?endif;?>
+                    <?if(!empty($arItem["PROPERTIES"]["LENGTH"]["VALUE"])):?>
+                    <span><?= $arItem["PROPERTIES"]["LENGTH"]["VALUE"] ?> м.</span>
+                    <?endif;?>
+                    <?if(!empty($arItem["PROPERTIES"]["MATERIAL"]["VALUE"])):?>
+                        <span><?= $arItem["PROPERTIES"]["MATERIAL"]["VALUE"] ?>.</span>
+                    <?endif;?>
+                    <?if(!empty($arItem["PROPERTIES"]["COLOR"]["VALUE"])):?>
+                        <span><?= $arItem["PROPERTIES"]["COLOR"]["VALUE"] ?>.</span>
+                    <?endif;?>
+                    <?if(!empty($arItem["PROPERTIES"]["CORNER"]["VALUE"])):?>
+                        <span><?= $arItem["PROPERTIES"]["CORNER"]["VALUE"] ?>.</span>
+                    <?endif;?>
+                    <?if(!empty($arItem["PROPERTIES"]["WIDTH"]["VALUE"])):?>
+                        <span><?= $arItem["PROPERTIES"]["WIDTH"]["VALUE"] ?>.</span>
+                    <?endif;?>
+                </div>
+            <?endif;?>
             <!--					<a href="-->
             <? //=$arItem["DETAIL_PAGE_URL"]?><!--" class="all-btn">Подробнее</a>-->
         </article>
     <? endforeach; ?>
 </div>
-
